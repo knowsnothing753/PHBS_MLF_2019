8d2c9951ac7d8779c208915a31260ec117401fe2
knowsnothing753 <1183282952@qq.com>
2020-04-24 13:13:44 +0800

    update

--- boxplot.R
+++
@@ -1,7 +0,0 @@
-etwd("~/Desktop/MLF project")
-a = read.csv("描述分析.csv")
-par(mfrow=c(2,2))
-boxplot(a$x_10_0_1~a$LABEL,xlab = '10year-3month',ylab = '')
-boxplot(a$x_20_1_1~a$LABEL,xlab = '20year-1year',ylab = '')
-boxplot(a$x_10_1_1~a$LABEL,xlab = '10year-1year',ylab = '')
-boxplot(a$x_20_0_1~a$LABEL,xlab = '20year-3month',ylab = '')
